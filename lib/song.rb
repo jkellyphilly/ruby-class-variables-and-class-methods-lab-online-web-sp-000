1cf9b83f0c3681329fb66198e17b8f881135aef6
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-30 19:01:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,9 +52,9 @@ class Song
     artist_count_hash = {}
     @@artists.each do |artist|
       if artist_count_hash.has_key?(artist)
-        artist_count_hash[genre] += 1
+        artist_count_hash[artist] += 1
       else
-        artist_count_hash[genre] = 1
+        artist_count_hash[artist] = 1
       end
     end
     artist_count_hash
