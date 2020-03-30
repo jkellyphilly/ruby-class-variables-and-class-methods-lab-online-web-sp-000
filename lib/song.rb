7e0f59bfd1e7f99884e40bd96c1c4c058d782ef1
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-30 18:59:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,10 @@ class Song
   def self.genre_count
     genre_count_hash = {}
     @@genres.each do |genre|
-
+      if genre_count_hash.has_key?(genre)
+        genre_count_hash[genre] += 1
+      else
+        genre_count_hash[genre] = 1
     end
     genre_count_hash
   end
